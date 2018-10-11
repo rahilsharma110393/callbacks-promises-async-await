cb5dcd061b636e4e9d6a21788f0b14536a199793
santhor <santhor@deloitte.com>
2018-10-11 11:31:36 +0530

    Added API request implementation with Promise and async

--- nasa-details.js
+++ nasa-details.js
@@ -1,17 +1,30 @@
 const axios = require('axios');
-const API = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+const NASA_API = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
 
-function getNasaDetails () {
-    axios.get(API)
-    	.then(response => {
-    		console.log('**** NASA information ****');
-    		console.log(response.data.explanation);
+function getNasaDetails (NASA_API) {
+    return axios.get(NASA_API);
+}
+
+function getNasaDetailsWithPromise () {
+	getNasaDetails (NASA_API)
+		.then(response => {
+    		console.log('NASA Info using promise: ', response.data.explanation);
     	})
-    	.catch(error => {
-    		console.log(error);
+    	.catch(err => {
+    		console.log('Error in retrieving the NASA info.', err);
     	});
 }
 
+async function getNasaDetailsWithAsync () {
+	try {
+		let response = await getNasaDetails(NASA_API);
+    	console.log('NASA Info using async: ', response.data.explanation);
+	} catch (err) {
+		console.log('Error in retrieving the NASA info.', err);
+	} 
+}
+
 module.exports = {
-    getNasaDetails
+	getNasaDetailsWithPromise,
+	getNasaDetailsWithAsync
 };
